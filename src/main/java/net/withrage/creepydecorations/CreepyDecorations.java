fc19d8edecc32b130198b0456d79df5d3eb86430
justeriksk <sebo.erik@gmail.com>
2025-04-22 16:36:02 +0200

    added custom creative tab for creepy decorations

--- src/main/java/net/withrage/creepydecorations/CreepyDecorations.java
+++ src/main/java/net/withrage/creepydecorations/CreepyDecorations.java
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.withrage.creepydecorations.block.ModBlocks;
+import net.withrage.creepydecorations.item.ModCreativeModeTabs;
 import net.withrage.creepydecorations.item.ModItems;
 import org.slf4j.Logger;
 
@@ -31,6 +32,9 @@ public class CreepyDecorations {
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
+
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
 
---
+++ src/main/java/net/withrage/creepydecorations/item/ModCreativeModeTabs.java
@@ -0,0 +1,32 @@
+package net.withrage.creepydecorations.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.withrage.creepydecorations.CreepyDecorations;
+import net.withrage.creepydecorations.block.ModBlocks;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CreepyDecorations.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> CREEPY_DECORATIONS_CREATIVE_TAB = CREATIVE_MODE_TABS.register("creepy_decorations_creative_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.HAUNTED_PUMPKIN.get()))
+                    .title(Component.translatable("creativetab.creepydecorations.creepy_decorations_creative_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModBlocks.HAUNTED_PUMPKIN.get());
+                        output.accept(ModBlocks.BROKEN_DOLL.get());
+                        output.accept(ModBlocks.GRAVESTONE.get());
+                        output.accept(ModBlocks.WOODEN_CROSS.get());
+                        output.accept(ModBlocks.LEG_SHACKLE.get());
+                        output.accept(ModBlocks.EMERGING_HAND.get());
+                    }).build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
