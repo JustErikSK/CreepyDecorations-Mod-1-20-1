04b810a880e6b9d0e049fef702c87eab94f9916f
justeriksk <sebo.erik@gmail.com>
2025-05-05 09:01:21 +0200

    every block now requires a correct tool for the most efficient mining, added json files for these tools

--- src/main/java/net/withrage/creepydecorations/block/ModBlocks.java
+++ src/main/java/net/withrage/creepydecorations/block/ModBlocks.java
@@ -18,22 +18,22 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, CreepyDecorations.MOD_ID);
 
     public static final RegistryObject<Block> HAUNTED_PUMPKIN = registerBlock("haunted_pumpkin", () -> new HauntedPumpkin(BlockBehaviour.Properties.of()
-            .noOcclusion().strength(2f).sound(SoundType.STEM).lightLevel(state -> 10)));
+            .noOcclusion().strength(2f).sound(SoundType.STEM).lightLevel(state -> 10).requiresCorrectToolForDrops()));
 
     public static final RegistryObject<Block> BROKEN_DOLL = registerBlock("broken_doll", () -> new BrokenDoll(BlockBehaviour.Properties.of()
-            .noOcclusion().strength(0.5f).sound(SoundType.WOOL)));
+            .noOcclusion().strength(0.5f).sound(SoundType.WOOL).requiresCorrectToolForDrops()));
 
     public static final RegistryObject<Block> MOSSY_GRAVESTONE = registerBlock("mossy_gravestone", () -> new MossyGravestone(BlockBehaviour.Properties.of()
-            .noOcclusion().strength(3f).sound(SoundType.DEEPSLATE)));
+            .noOcclusion().strength(3f).sound(SoundType.DEEPSLATE).requiresCorrectToolForDrops()));
 
     public static final RegistryObject<Block> WOODEN_CROSS = registerBlock("wooden_cross", () -> new WoodenCross(BlockBehaviour.Properties.of()
-            .noOcclusion().strength(2f).sound(SoundType.WOOD)));
+            .noOcclusion().strength(2f).sound(SoundType.WOOD).requiresCorrectToolForDrops()));
 
     public static final RegistryObject<Block> LEG_SHACKLE = registerBlock("leg_shackle", () -> new LegShackle(BlockBehaviour.Properties.of()
-            .noOcclusion().strength(5f).sound(SoundType.CHAIN)));
+            .noOcclusion().strength(5f).sound(SoundType.CHAIN).requiresCorrectToolForDrops()));
 
     public static final RegistryObject<Block> EMERGING_HAND = registerBlock("emerging_hand", () -> new EmergingHand(BlockBehaviour.Properties.of()
-            .noOcclusion().strength(0.5f).sound(SoundType.MUD)));
+            .noOcclusion().strength(0.5f).sound(SoundType.MUD).requiresCorrectToolForDrops()));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> blockSupplier) {
         RegistryObject<T> block = ModBlocks.BLOCKS.register(name, blockSupplier);
