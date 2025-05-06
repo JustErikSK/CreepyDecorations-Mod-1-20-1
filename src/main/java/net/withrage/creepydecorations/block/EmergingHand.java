2f62383d2d79214f8bb72ab93c16362369d1b87f
justeriksk <sebo.erik@gmail.com>
2025-05-06 10:31:56 +0200

    added a zoglin ambient sound for emerging hand upon being destroyed, version is now 1.0.3

--- src/main/java/net/withrage/creepydecorations/block/EmergingHand.java
+++ src/main/java/net/withrage/creepydecorations/block/EmergingHand.java
@@ -1,7 +1,11 @@
 package net.withrage.creepydecorations.block;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -13,6 +17,23 @@ public class EmergingHand extends Block {
     public EmergingHand(Properties properties) {
         super(properties);
         this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    public void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean isMoving) {
+        if (!state.is(newState.getBlock())) {
+            if (!level.isClientSide) {
+                level.playSound(
+                        null,
+                        pos,
+                        SoundEvents.ZOGLIN_AMBIENT,
+                        SoundSource.BLOCKS,
+                        1.0F,
+                        1.0F
+                );
+            }
+            super.onRemove(state, level, pos, newState, isMoving);
+        }
     }
 
     @Override
